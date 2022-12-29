f4bdd94777027263a43b356fb1c5b8f600a242be
K. M. Soriful Islam Adnan <adnanfaith7@gmail.com>
2022-12-29 22:51:49 +0600

    task post api created

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
+const app = express();
 const port = process.env.PORT || 5000;
 
-const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 // Middleware
 app.use(cors());
@@ -14,18 +14,82 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3aa5vlu.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+       
+        // Database Collections
 
+        const taskCollection = client.db("smartTaskManagerTool").collection("tasks");
+
+        // Task API
+
+        app.post('/task', async (req, res) => {
+            const task = req.body;
+            console.log(task);
+            const result = await taskCollection.insertOne(task);
+            res.send(result);
+        });
+
+        // app.patch('/task/:id', async (req, res)=>{
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id) }
+        //     const advertise = req.body.advertise;
+        //     const updatedDoc = {
+        //         $set: {
+        //             advertise: 'True',
+        //         }
+        //     }
+        //     const result = await taskCollection.updateOne(query, updatedDoc);
+        //     res.send(result);
+        // });
+
+        // app.get('/products', async (req, res)=>{
+        //     let query = {};
+
+        //     if(req.query.category){
+        //         query= {
+        //             category: req.query.category
+        //         }
+        //     }
+
+        //     const cursor = productsCollection.find(query);
+        //     const products = await cursor.toArray();
+        //     res.send(products);
+        // })
+
+        // app.get('/my-products', async (req, res) => {
+
+        //     let query = {};
+
+        //     if(req.query.sellerEmail){
+        //         query= {
+        //             sellerEmail: req.query.sellerEmail
+        //         }
+        //     }
+
+        //     const cursor = productsCollection.find(query);
+        //     const products = await cursor.toArray();
+        //     res.send(products);
+        // });
+
+        // app.delete('/product/:id', async (req, res)=>{
+        //     const id = req.params.id;
+        //     const query = {_id:ObjectId(id)};
+        //     const result = await productsCollection.deleteOne(query);
+        //     res.send(result);
+        // });
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.log(err));
 
 app.get('/', (req, res) =>{
     res.send('Smart task manager lool running');
 });
-
-
-
-
-
-
-
 
 app.listen(port, ()=>{
     console.log(`Smart task manager lool running on ${port}`);
