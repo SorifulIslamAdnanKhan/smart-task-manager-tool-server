4fd0dec0d8b2badeb606886dde031e29556d0b92
K. M. Soriful Islam Adnan <adnanfaith7@gmail.com>
2022-12-29 09:38:42 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const port = process.env.PORT || 5000;
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3aa5vlu.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 
 app.get('/', (req, res) =>{
