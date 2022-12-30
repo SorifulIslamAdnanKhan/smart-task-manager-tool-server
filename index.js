9ea4288a4154fd417bebac23a708fadb895c8cdf
K. M. Soriful Islam Adnan <adnanfaith7@gmail.com>
2022-12-30 23:32:03 +0600

    all api created

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, MongoRuntimeError, ObjectId } = require('mongodb');
+
 require('dotenv').config();
 
 // Middleware
@@ -20,64 +21,129 @@ async function run() {
         // Database Collections
 
         const taskCollection = client.db("smartTaskManagerTool").collection("tasks");
+        const commentCollection = client.db("smartTaskManagerTool").collection("comments");
 
         // Task API
 
         app.post('/task', async (req, res) => {
             const task = req.body;
-            console.log(task);
             const result = await taskCollection.insertOne(task);
             res.send(result);
         });
 
-        // app.patch('/task/:id', async (req, res)=>{
-        //     const id = req.params.id;
-        //     const query = { _id: ObjectId(id) }
-        //     const advertise = req.body.advertise;
-        //     const updatedDoc = {
-        //         $set: {
-        //             advertise: 'True',
-        //         }
-        //     }
-        //     const result = await taskCollection.updateOne(query, updatedDoc);
-        //     res.send(result);
-        // });
+        app.get('/my-tasks', async (req, res) => {
 
-        // app.get('/products', async (req, res)=>{
-        //     let query = {};
+            let query = {};
 
-        //     if(req.query.category){
-        //         query= {
-        //             category: req.query.category
-        //         }
-        //     }
+            if(req.query.userEmail){
+                query= {
+                    userEmail: req.query.userEmail
+                }
+            }
 
-        //     const cursor = productsCollection.find(query);
-        //     const products = await cursor.toArray();
-        //     res.send(products);
-        // })
+            const cursor = taskCollection.find(query);
+            const tasks = await cursor.toArray();
+            res.send(tasks);
+        });
 
-        // app.get('/my-products', async (req, res) => {
+        app.delete('/task/:id', async (req, res)=>{
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)};
+            const result = await taskCollection.deleteOne(query);
+            res.send(result);
+        });
 
-        //     let query = {};
+        app.get('/task/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const result = await taskCollection.findOne(filter)
+            res.send(result);
+        })
 
-        //     if(req.query.sellerEmail){
-        //         query= {
-        //             sellerEmail: req.query.sellerEmail
-        //         }
-        //     }
+        app.patch('/task/:id', async (req, res)=>{
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const task = req.body;
+            const updatedDoc = {
+                $set: {
+                    name: task.name,
+                    image: task.image,
+                }
+            }
+            const result = await taskCollection.updateOne(query, updatedDoc);
+            res.send(result);
+        });
 
-        //     const cursor = productsCollection.find(query);
-        //     const products = await cursor.toArray();
-        //     res.send(products);
-        // });
+        app.put('/task/:id', async (req, res)=>{
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const complete = req.body.complete;
+            const option = {upsert: true}
+            const updatedDoc = {
+                $set: {
+                    complete: 'True',
+                }
+            }
+            const result = await taskCollection.updateOne(query, updatedDoc, option);
+            res.send(result);
+        });
 
-        // app.delete('/product/:id', async (req, res)=>{
-        //     const id = req.params.id;
-        //     const query = {_id:ObjectId(id)};
-        //     const result = await productsCollection.deleteOne(query);
-        //     res.send(result);
-        // });
+        app.get('/tasks/complete', async (req, res)=>{
+            const complete = req.params.complete;
+            const query = {complete: 'True'};
+            const result = await taskCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.delete('/tasks/complete/:id', async (req, res)=>{
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)};
+            const result = await taskCollection.deleteOne(query);
+            res.send(result);
+        });
+
+        app.get('/tasks/complete/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const result = await taskCollection.findOne(filter)
+            res.send(result);
+        })
+
+        app.patch('/tasks/complete/:id', async (req, res)=>{
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const complete = req.body.complete;
+            const updatedDoc = {
+                $set: {
+                    complete: 'False',
+                }
+            }
+            const result = await taskCollection.updateOne(query, updatedDoc);
+            res.send(result);
+        });
+
+        // Comment API
+
+        app.post('/comment', async (req, res) => {
+            const task = req.body;
+            const result = await commentCollection.insertOne(task);
+            res.send(result);
+        });
+
+        app.get('/my-comments', async (req, res) => {
+
+            let query = {};
+
+            if(req.query.userEmail){
+                query= {
+                    userEmail: req.query.userEmail
+                }
+            }
+
+            const cursor = commentCollection.find(query);
+            const comments = await cursor.toArray();
+            res.send(comments);
+        });
 
     }
     finally {
